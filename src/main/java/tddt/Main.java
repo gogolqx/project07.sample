53ab54a06b81dfa8aa9082b940162210c585ea69
Joyce <lqx@apples-MacBook-Pro-2.local>
2016-06-28 21:23:27 +0200

    Initial commit

---
+++ src/main/java/tddt/Main.java
@@ -0,0 +1,8 @@
+package tddt;
+
+/**
+ * Created by lqx on 28/06/16.
+ */
+public class Main {
+    public void sampleMethod(){}
+}
---
+++ src/test/java/TestSample.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lqx on 28/06/16.
+ */
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestSample {
+
+
+    @Test
+    public void testSampleMethod() {
+
+    }
+
+}
